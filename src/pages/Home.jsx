6fd022924a679425a3379b85a9a75041a3d08f6a
Alkis <alkis.k1972@gmail.com>
2025-10-26 21:11:25 +0200

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import MovieCard from '../components/MovieCard';
+import { useState } from 'react';
+
+function Home() {
+    const movies = [
+        { id: 1, title: 'John Wick', release_date: '2020' },
+        { id: 2, title: 'Terminator', release_date: '1999' },
+        { id: 3, title: 'The Matrix', release_date: '1998' },
+    ];
+
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        setSearchQuery('');
+        console.log(searchQuery);
+    };
+
+    return (
+        <div className='home'>
+            <form onSubmit={handleSearch} className='search-form'>
+                <input
+                    type='text'
+                    placeholder='Search for movies...'
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    className='search-input'
+                />
+                <button type='submit' className='search-button'>
+                    Search
+                </button>
+            </form>
+            <div className='movies-grid'>
+                {movies.map((movie) => (
+                    <MovieCard movie={movie} key={movie.id} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Home;
