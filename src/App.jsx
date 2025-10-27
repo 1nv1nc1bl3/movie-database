272654297380ffca0fd8a058e75cc15a11f72326
Alkis <alkis.k1972@gmail.com>
2025-10-27 20:49:13 +0200

    fetched movies form api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import Home from './pages/Home';
 import './css/App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Favorites from './pages/Favorites';
+import NavBar from './components/NavBar';
 
 export default function App() {
     return (
-        <>
-            <Home />
-        </>
+        <div>
+            <NavBar />
+            <main className='main-content'>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/favorites' element={<Favorites />} />
+                </Routes>
+            </main>
+        </div>
     );
 }
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,4 +1,12 @@
-function Favorites() {
-    return <div>Favorites</div>;
+import '../css/Favorites.css';
+
+export default function Favorites() {
+    return (
+        <div className='favorites-empty'>
+            <h2>No favorite movies yet</h2>
+            <p>
+                Start adding movies to your favorites and they will appear here.
+            </p>
+        </div>
+    );
 }
-export default Favorites;
